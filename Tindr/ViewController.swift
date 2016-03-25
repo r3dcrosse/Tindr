2d7f00b6488b28e2ddd4eb9e7c9b63d08eefc04f
r3dcrosse <mrdavidwayman@gmail.com>
2016-03-24 19:19:49 -0700

    First commit

---
+++ Tindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tindr
+//
+//  Created by David Wayman on 3/24/16.
+//  Copyright © 2016 David Wayman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
